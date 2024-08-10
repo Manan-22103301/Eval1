4e9c4ee40a6429d6a4180339b9d29b70533570c9
Manan-22103301 <22103301@mail.jiit.ac.in>
2024-08-10 10:22:23 +0530

    Create SETA_Q2.c

---
+++ SETA_Q2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  char str[100],temp;
+  int i,j;
+
+  printf("Enter a string:");
+  fgets(str, sizeof(str), stdin)
+
+  
+
+}
