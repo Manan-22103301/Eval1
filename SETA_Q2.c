71e1825e751f3b044983353f378fd66f8dd0211a
Manan-22103301 <22103301@mail.jiit.ac.in>
2024-08-10 10:42:56 +0530

    Update SETA_Q2.c

--- SETA_Q2.c
+++ SETA_Q2.c
@@ -1,16 +1,42 @@
-#include <stdio.h>
-#include <string.h>
 #include <iostream>
-using namespace std;
+#include <map>
+#include <cctype>
+#include <string>
 
-int main(){
-
-  char str[100],temp;
-  int i,j;
-
-  printf("Enter a string:");
-  fgets(str, sizeof(str), stdin)
-
-  
-
+// Function to check if a character is a letter
+bool isLetter(char ch) {
+    return std::isalpha(static_cast<unsigned char>(ch));
 }
+
+// Function to convert a character to lowercase
+char toLower(char ch) {
+    return std::tolower(static_cast<unsigned char>(ch));
+}
+
+int main() {
+    // Read the paragraph from the user
+    std::string paragraph;
+    std::cout << "Enter a paragraph: ";
+    std::getline(std::cin, paragraph);
+
+    // Map to store character frequencies
+    std::map<char, int> freqMap;
+
+    // Iterate through each character in the paragraph
+    for (char ch : paragraph) {
+        if (isLetter(ch)) {
+            char lowerCh = toLower(ch);
+            freqMap[lowerCh]++;
+        }
+    }
+
+    // Print the frequencies
+    std::cout << "Character frequencies:\n";
+    for (const auto& entry : freqMap) {
+        std::cout << entry.first << ": " << entry.second << "\n";
+    }
+
+    return 0;
+}
+
+
